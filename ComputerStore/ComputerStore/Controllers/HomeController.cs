1329ed8e4ff8934951a595f0f5635ef81ae65abc
Lee Cottrell <44240965+leecottrellPTC@users.noreply.github.com>
2022-10-17 13:28:29 -0400

    Computer Store 1
    Created a project. Added two views. Edited the HomeController. Demonstrated ViewData.

---
+++ ComputerStore/ComputerStore/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using ComputerStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ComputerStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Laptops()
+        {
+            return View();
+        }
+
+        public IActionResult Desktops()
+        {
+            //enter logic to display seasonal message
+            ViewData["Message"] = "Desktop Computers for sale!";
+            ViewData["CrazyEddie"] = "15% off gaming rigs";
+            return View(new string[] {"Gaming desktop", "Office desktop", "Take over the world"});
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
